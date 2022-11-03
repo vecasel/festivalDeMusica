5c888d77e75ad1063409ae3d7df7f064da351110
Vecasel <danife1986@hotmail.com>
2022-11-02 23:44:51 -0500

    4th change

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-const {src, dest, watch} = require("gulp");
+//Gulp
+const {src, dest, watch, parallel} = require("gulp");
+//Css
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+//Images
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
+
 
 function css(callback){
     src("src/scss/**/*.scss")
@@ -11,10 +19,50 @@ function css(callback){
     callback();
 }
 
+function images(callback){
+    const options = {
+        optimizationLevel : 3
+    };
+    src("src/img/**/*.{png,jpg}")
+        .pipe(cache(imagemin(options)))
+        .pipe(dest("build/img"));
+    callback();
+}
+
+function versionWebp(callback){
+    const options = {
+        quality: 50
+    };
+    src("src/img/**/*.{png,jpg}")
+        .pipe(webp(options))
+        .pipe(dest("build/img"));
+    callback();
+}
+
+function versionAvif(callback){
+    const options = {
+        quality: 50
+    };
+    src("src/img/**/*.{png,jpg}")
+        .pipe(avif(options))
+        .pipe(dest("build/img"));
+    callback();
+}
+
+function javaScript(callback){
+    src("src/js/**/*.js")
+        .pipe(dest("build/js"));
+    callback();
+}
+
 function dev(callback){
     watch("src/scss/**/*.scss", css);
-
+    watch("src/js/**/*.js", javaScript);
     callback();
 }
 exports.css = css;
-exports.dev = dev;
+exports.js = javaScript;
+exports.images = images
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(images, versionWebp, versionAvif, dev);
